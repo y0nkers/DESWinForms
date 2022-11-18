476c0d462790c458d04339f3a2d11faed8991016
Evgeniy <75331348+y0nkers@users.noreply.github.com>
2022-11-18 18:31:41 +0300

    Added shift register for IV and block size. Deleted fucking logs.

--- src/DES.hpp
+++ src/DES.hpp
@@ -2,6 +2,7 @@
 #define DES_HPP
 
 #include "helpers.hpp"
+#include "boost/dynamic_bitset.hpp"
 
 namespace DESWinForms {
 	using namespace System;
@@ -12,19 +13,20 @@ namespace DESWinForms {
 		DECRYPTION = 2
 	};
 
-	// Key size - 56 (extra 8 for error detection) bits, Block size - 64 bits
+	// Key size - 56 (extra 8 for error detection) bits, Block size - 64 bits, 16 48bit round keys, plaintext block size 8, 16, ..., 64 bits
 	class DES {
 	private:
 		std::bitset<64> key;
 		std::bitset<64> IV;
 		std::bitset<48> subkeys[16];
+		unsigned int blockSize;
 
 		static void leftShift(std::bitset<28>& bits, unsigned count);
-		void generateSubKeys(std::string key_string, TextBox^ tbLog);
-		std::bitset<32> feistel(std::bitset<32>& block_R, std::bitset<48> subkey, unsigned round, TextBox^ tbLog);
+		void generateSubkeys(std::string key_string);
+		std::bitset<32> feistel(std::bitset<32>& block_R, std::bitset<48> subkey, unsigned round);
 	public:
-		DES(String^ keyStr, String^ ivStr);
-		String^ process(String^ text, Mode mode, TextBox^ tbLog);
+		DES(String^ keyStr, String^ ivStr, int blockSize);
+		String^ process(String^ text, Mode mode);
 		static std::string generateBytes(bool generateKey);
 	};
 
